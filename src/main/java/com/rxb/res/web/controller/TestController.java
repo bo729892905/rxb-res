95026ced3484161aa4380833b73b290ff4cdac84
bo729892905 <wonderfulxiaobo@gmail.com>
2016-02-28 10:34:08 +0800

    添加metric

--- src/main/java/com/rxb/res/web/controller/TestController.java
+++ src/main/java/com/rxb/res/web/controller/TestController.java
@@ -11,13 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
@@ -104,5 +102,18 @@ public class TestController {
 	public @ResponseBody String testConverter(Map<String,Object> map) {
 		map.put("name", "tom");
 		return "success";
+	}
+
+	@RequestMapping(value = "{reqName}.htm")
+	public ModelAndView testFreemarker(HttpServletRequest request, @PathVariable String reqName) {
+		ModelAndView mav=new ModelAndView("menu");
+		String src = "#";
+		if(reqName.equals("baidu")){
+			src = "http://www.baidu.com";
+		}else {
+			src = "http://www.taobao.com";
+		}
+		mav.addObject("src", src);
+		return mav;
 	}
 }
